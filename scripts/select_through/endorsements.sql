2328040f22d8494204897afd7eeb56b31ec4dda0
J-Weeks <weeksjasons@gmail.com>
2016-04-25 15:54:55 -0400

    builds out solution scripts

--- scripts/select_through/endorsements.sql
+++ scripts/select_through/endorsements.sql
@@ -1,1 +1,7 @@
 -- get information using endorsements join table
+select p.given_name, p.surname
+  from people p
+    inner join endorsement e on e.person_id = p.id
+    inner join skills s on e.skills_id = s.id
+  where s.framework = 'Ember.js'
+;
